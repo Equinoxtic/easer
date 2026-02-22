5eff10991f85a00042663cb52df9cf1af5bcc14a
Equinoxtic <thisistherealequinoctic@gmail.com>
2026-02-22 23:18:29 +0800

    refactor: Update Registry `subscribe()` method and add documentation

--- source/modcharting/Registry.hx
+++ source/modcharting/Registry.hx
@@ -21,9 +21,19 @@ class Registry {
 		ModIDs.CENTERROTATE => {}
 	];
 	
+	/**
+		Registers a list of modifiers for a modchart manager.
+		@param modMap The list of modifiers to apply
+		@param manager The manager instace for modcharts
+		@returns `Map<String, Dynamic>`
+	**/
 	public static function subscribe(modMap:Map<String, Dynamic>, manager:String):Map<String, Dynamic> {
 		for (mod => data in modMap) {
 			manager.addModifier(mod);
+			if (data.value != null) {
+				var field:Int = (data.player != null) ? data.player : -1;
+				manager.setPercent(mod, data.value, field);
+			}
 		}
 		return modMap;
 	}
