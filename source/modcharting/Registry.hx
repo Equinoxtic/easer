1fd60cfbcb59b82fcfbbb9e4573d21107b0ecf98
Equinoxtic <thisistherealequinoctic@gmail.com>
2026-02-23 01:32:24 +0800

    gotta delete that, hehe

--- source/modcharting/Registry.hx
+++ source/modcharting/Registry.hx
@@ -3,24 +3,6 @@ package modcharting;
 import modcharting.ModIDs;
 
 class Registry {
-	public static final HJOIM_MODS:Map<String, Dynamic> = [
-		ModIDs.TIPSY => {},
-		ModIDs.DRUNK => {},
-		ModIDs.BEAT => {},
-		ModIDs.INVERT => {},
-		ModIDs.OPPONENTSWAP => {},
-		ModIDs.TRANSFORM => {},
-		ModIDs.REVERSE => {},
-		ModIDs.SPIRAL => {},
-		ModIDs.BEAT => {},
-		ModIDs.ROTATE => {},
-		ModIDs.SCALE => {},
-		ModIDs.ZOOM => {},
-		ModIDs.BOUNCE => {},
-		ModIDs.TORNADO => {},
-		ModIDs.CENTERROTATE => {}
-	];
-	
 	/**
 		Registers a list of modifiers for a modchart manager.
 		@param modMap The list of modifiers to apply
