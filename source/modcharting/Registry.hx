da44aaf5af7e7524576a650a3c7b42127ef3bd1f
Equinoxtic <thisistherealequinoctic@gmail.com>
2026-02-24 12:14:05 +0800

    feat: Add basic/preset list of modifiers in Registry

--- source/modcharting/Registry.hx
+++ source/modcharting/Registry.hx
@@ -3,6 +3,24 @@ package modcharting;
 import modcharting.ModIDs;
 
 class Registry {
+	public static final MODS:Map<String, Dynamic> = [
+		ModIDs.TIPSY => {},
+		ModIDs.DRUNK => {},
+		ModIDs.BEAT => {},
+		ModIDs.INVERT => {},
+		ModIDs.OPPONENTSWAP => {},
+		ModIDs.TRANSFORM => {},
+		ModIDs.REVERSE => {},
+		ModIDs.SPIRAL => {},
+		ModIDs.BEAT => {},
+		ModIDs.ROTATE => {},
+		ModIDs.SCALE => {},
+		ModIDs.ZOOM => {},
+		ModIDs.BOUNCE => {},
+		ModIDs.TORNADO => {},
+		ModIDs.CENTERROTATE => {}
+	];
+	
 	/**
 		Registers a list of modifiers for a modchart manager.
 		@param modMap The list of modifiers to apply
